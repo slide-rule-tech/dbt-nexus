8bccd8d80156cde44826d3675d1768afecc0f437
Kevin McLaughlin <github@kevincmclaughlin.com>
2025-10-20 14:57:56 -0500

    cleanup

--- models/nexus-models/final-tables/compatibility/nexus_group_participants.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view', tags=['compatibility']) }}
-
-select
-    entity_participant_id as group_participant_id,
-    event_id,
-    entity_id as group_id,
-    role
-from {{ ref('nexus_entity_participants') }}
-where entity_type = 'group'
--- models/nexus-models/final-tables/compatibility/nexus_person_participants.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view', tags=['compatibility']) }}
-
-select
-    entity_participant_id as person_participant_id,
-    event_id,
-    entity_id as person_id,
-    role
-from {{ ref('nexus_entity_participants') }}
-where entity_type = 'person'
