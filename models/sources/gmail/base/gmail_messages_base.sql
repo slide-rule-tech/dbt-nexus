be4a87b79410c419cb01baecf1d6683a2b7a3c68
Kevin McLaughlin <github@kevincmclaughlin.com>
2025-10-29 13:24:33 -0500

    u

--- models/sources/gmail/base/gmail_messages_base.sql
+++ models/sources/gmail/base/gmail_messages_base.sql
@@ -1,1 +1,8 @@
+{{ config(
+    enabled=var('nexus', {}).get('sources', {}).get('gmail', {}).get('enabled', false),
+    materialized='view',
+    tags=['gmail', 'base']
+) }}
+
+-- Base layer: Raw table with zero transformation overhead
 select * from {{ source('gmail', 'messages') }}
