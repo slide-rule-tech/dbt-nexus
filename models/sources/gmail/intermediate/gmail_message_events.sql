a45875b2c7bb2143224b28be4b59d025eeaea15a
Kevin McLaughlin <github@kevincmclaughlin.com>
2026-03-04 10:50:43 -0600

    feat: gmail event significance [WORK-2607]

--- models/sources/gmail/intermediate/gmail_message_events.sql
+++ models/sources/gmail/intermediate/gmail_message_events.sql
@@ -11,6 +11,10 @@ SELECT
     'message sent' as event_name,
     'email' as event_type,
     subject as event_description,
+    CASE
+        WHEN COALESCE(is_automated_or_bulk_message, FALSE) THEN -10
+        ELSE 0
+    END as significance,
     'gmail' as source,
     'gmail_message_events' as source_table,
     _ingested_at,
@@ -23,6 +27,14 @@ SELECT
     sent_at,
     subject,
     in_reply_to,
+    auto_submitted_header,
+    precedence_header,
+    list_id_header,
+    list_unsubscribe_header,
+    x_auto_response_suppress_header,
+    x_autoreply_header,
+    x_autorespond_header,
+    is_automated_or_bulk_message,
     raw_subject,
     snippet,
     size_estimate,
